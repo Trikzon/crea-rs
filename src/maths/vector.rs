cb18da0cb9ea8f9b98747dec9e074f8bb8dd54c0
Trikzon <trikzondev@gmail.com>
2021-02-26 20:50:46 -0500

    Add more vector functions

--- src/maths/vector.rs
+++ src/maths/vector.rs
@@ -8,24 +8,32 @@ pub type Component = f32;
 
 pub trait Vector:
     'static
-    + Send
-    + Sync
-    + Sized
     + Copy
     + Clone
     + PartialEq
     + fmt::Display
     + ops::Neg<Output = Self>
     + ops::Add<Output = Self>
+    + ops::Add<Component, Output = Self>
     + ops::AddAssign
+    + ops::AddAssign<Component>
     + ops::Sub<Output = Self>
+    + ops::Sub<Component, Output = Self>
     + ops::SubAssign
+    + ops::SubAssign<Component>
     + ops::Mul<Component, Output = Self>
     + ops::MulAssign<Component>
     + ops::Div<Component, Output = Self>
     + ops::DivAssign<Component>
 {
     fn zero() -> Self;
+
+    fn length_squared(&self) -> f32;
+    fn length(&self) -> f32;
+
+    fn normalized(&self) -> Self;
+
+    fn dot_product(&self, rhs: &Self) -> Component;
 }
 
 macro_rules! vec_type {
@@ -50,6 +58,23 @@ macro_rules! vec_type {
         impl Vector for $type {
             fn zero() -> Self {
                 Self { $($component: 0.0),* }
+            }
+
+            fn length_squared(&self) -> f32 {
+                self.dot_product(&self)
+            }
+
+            fn length(&self) -> f32 {
+                self.length_squared().sqrt()
+            }
+
+            fn normalized(&self) -> Self {
+                let length = self.length();
+                Self { $($component: self.$component / length),* }
+            }
+
+            fn dot_product(&self, rhs: &Self) -> Component {
+                [$(self.$component * rhs.$component),*].iter().fold(0.0, |a, &b| a + b)
             }
         }
 
@@ -88,7 +113,9 @@ macro_rules! vec_type {
         }
 
         bin_op!(cwise, Add, add, +);
+        bin_op!(linear, Add, add, +);
         bin_op!(cwise, Sub, sub, -);
+        bin_op!(linear, Sub, sub, -);
         bin_op!(linear, Mul, mul, *);
         bin_op!(linear, Div, div, /);
 
@@ -96,7 +123,7 @@ macro_rules! vec_type {
             (cwise $rhs:ty, $op_name:ident, $fn:ident, $op:tt) => {
                 impl ops::$op_name<$rhs> for $type {
                     fn $fn(&mut self, rhs: $rhs) {
-                    $(self.$component $op rhs.$component;)*
+                        $(self.$component $op rhs.$component;)*
                     }
                 }
             };
@@ -113,8 +140,11 @@ macro_rules! vec_type {
                 }
             };
         }
+
         bin_op_assign!(cwise, AddAssign, add_assign, +=);
+        bin_op_assign!(linear, AddAssign, add_assign, +=);
         bin_op_assign!(cwise, SubAssign, sub_assign, -=);
+        bin_op_assign!(linear, SubAssign, sub_assign, +=);
         bin_op_assign!(linear, MulAssign, mul_assign, *=);
         bin_op_assign!(linear, DivAssign, div_assign, /=);
     };
@@ -147,5 +177,31 @@ impl Vector3 {
 
     pub fn negative_z() -> Self {
         Self::new(0.0, 0.0, -1.0)
+    }
+
+    pub fn cross_product(&self, rhs: &Self) -> Self {
+        Self {
+            x: (self.y * rhs.z) - (self.z * rhs.y),
+            y: (self.z * rhs.x) - (self.x * rhs.z),
+            z: (self.x * rhs.y) - (self.y * rhs.x),
+        }
+    }
+}
+
+impl From<&Vector2> for (Component, Component) {
+    fn from(vec: &Vector2) -> Self {
+        (vec.x, vec.y)
+    }
+}
+
+impl From<&Vector3> for (Component, Component, Component) {
+    fn from(vec: &Vector3) -> Self {
+        (vec.x, vec.y, vec.z)
+    }
+}
+
+impl From<&Vector4> for (Component, Component, Component, Component) {
+    fn from(vec: &Vector4) -> Self {
+        (vec.x, vec.y, vec.z, vec.w)
     }
 }
